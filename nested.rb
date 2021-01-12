698e1afca4e106f55b552e0712941717c1b4ca42
kooky-range-8610 <dreedsanders@gmail.com>
2021-01-12 20:36:57 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -22,7 +22,7 @@ end
 def alan_kay_is_known_for
 	# What combination of keys would you use to return the value of the :known_for key of :alan_kay?
 	
-	programmer_hash = 
+	programmer_hash1 = 
  		{
       :grace_hopper => {
         :known_for => "COBOL",
