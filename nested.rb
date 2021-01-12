bc61e1f2c949cf3005bc6c55e9f0a8e9d0cc34e8
kooky-range-8610 <dreedsanders@gmail.com>
2021-01-12 20:35:15 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -111,7 +111,9 @@ def changing_alan
       }
     }
 programmer_hash[:alan_kay][:known_for] = "GUI"
+
 end
+return programmer_hash
 
 def adding_to_dennis
   # add "Assembly" to Dennis Ritchie's languages array
